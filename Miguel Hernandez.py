73ff876f601e2c559a43aa9691f950a14a4c3e0e
Miguel_SHB <>
2025-10-31 07:50:10 -0500

    Subida de archivos
    se creo el archivo de lista en .Python

---
+++ Miguel Hernandez.py
@@ -0,0 +1,19 @@
+print("Miguel_Hernandez") 
+name="Stiben_Ballen"
+years=17 
+print(name)
+numero1= 2
+numero2= 3
+suma=numero1+numero2
+print(suma)
+numero3= 5
+numero4= 10
+resta=numero3-numero4
+print(resta)
+multiplicacion= numero4*numero3
+print(multiplicacion)
+divicion=numero2/numero3
+print(divicion)
+entrada=input("escriba_un_numero")
+print(entrada)
+numero1=int(entrada)
---
+++ lista.py
@@ -0,0 +1,8 @@
+lista=[]
+#lista.insert(4)
+#lista.insert(5)
+#lista.insert(6)
+lista.append(7)
+lista.append(8)
+lista.append(9)
+print(lista)
